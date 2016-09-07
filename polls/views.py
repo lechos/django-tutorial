63b8f4c9c2236293ba9efb5ea9bc756c54204c99
lechos <linneasaltel@gmail.com>
2016-09-07 05:49:04 +0000

    template system/namespacing

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 # Create your views here.
 from django.http import HttpResponse
@@ -16,10 +16,7 @@ def index(request):
   # return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
-  try: 
-    question = Question.objects.get(pk=question_id)
-  except Question.DoesNotExist:
-    raise Http404("Question does not exist")
+  question = Question.objects.get(pk=question_id)
   return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
